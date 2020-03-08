395ad0e1fc0dfca45d83bb0cb2280fbe4b95d3ec
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2020-03-08 15:18:34 +0100

    adding + removing rows

--- src/invoice.ts
+++ src/invoice.ts
@@ -1,17 +1,67 @@
 import { getValueByName } from './utils/dom'
 
-document
-  .querySelectorAll('[name=net], [name=quantity], [name=vat]')
-  .forEach(input => {
-    input.addEventListener('input', event => {
-      const trEl = (event.target as HTMLInputElement).closest('tr')!
+const tbody = document.querySelector('tbody')!
 
-      const gross = ['net', 'vat', 'quantity']
-        .map(name => getValueByName(name, trEl))
-        .reduce((acc, val) => acc * val, 1)
+addItem()
 
-      trEl.querySelector<HTMLInputElement>(
-        '[name=gross]',
-      )!.value = gross.toFixed(2)
-    })
-  })
+tbody.addEventListener('input', event => {
+  const target = event.target as HTMLInputElement
+
+  if (['net', 'vat', 'quantity'].includes(target.name)) {
+    const trEl = (event.target as HTMLInputElement).closest('tr')!
+
+    const gross = ['net', 'vat', 'quantity']
+      .map(name => getValueByName(name, trEl))
+      .reduce((acc, val) => acc * val, 1)
+
+    trEl.querySelector<HTMLInputElement>('[name=gross]')!.value = gross.toFixed(
+      2,
+    )
+  }
+})
+
+tbody.addEventListener('click', event => {
+  const el = event.target as HTMLElement
+
+  switch (el.dataset.action) {
+    case 'add':
+      addItem()
+      enumerate()
+      break
+
+    case 'remove':
+      removeItem(el)
+      if (tbody.childElementCount === 1) {
+        addItem()
+      }
+      enumerate()
+      break
+  }
+
+  el.dataset.action && event.preventDefault()
+})
+
+function enumerate() {
+  tbody
+    .querySelectorAll('tr')
+    .forEach(
+      (row, idx) =>
+        ((row as HTMLTableRowElement).querySelector<HTMLDivElement>(
+          'td div',
+        )!.innerText = `${idx}.`),
+    )
+}
+
+function removeItem(el: HTMLElement) {
+  el.closest('tr')!.remove()
+}
+
+function addItem() {
+  const newTr = tbody
+    .querySelector('.template')!
+    .cloneNode(true) as HTMLTableRowElement
+
+  newTr.classList.remove('template')
+
+  tbody.insertAdjacentElement('beforeend', newTr)
+}
